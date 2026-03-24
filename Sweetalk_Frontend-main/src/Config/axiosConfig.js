b93afbafe19099a162795f3b9bdc124ab8f2faa4
keyur2004http <kidechakeyur@123gmail.com>
2026-03-24 23:05:00 +0530

    Initial commit: Added frontend and backend structures

---
+++ Sweetalk_Frontend-main/src/Config/axiosConfig.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import nprogress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+nprogress.configure({ 
+  showSpinner: false,     // Disable the spinning wheel
+  speed: 500,             // Animation speed in ms
+  minimum: 0.2,           // Minimum percentage to start at
+  trickleSpeed: 200,      // How often to trickle progress
+  easing: 'ease',         // CSS easing string
+});
+
+const api = axios.create({
+    baseURL: 'http://localhost:8080'
+});
+
+api.interceptors.request.use(
+  (config) => {
+    nprogress.start();
+    const token = localStorage.getItem("token");
+    if (token && token.split(".").length === 3) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    nprogress.done();
+    return Promise.reject(error); 
+  }
+);
+
+api.interceptors.response.use(
+    (response) => {
+      nprogress.done(); 
+      return response;
+    },
+    (error) => {
+      nprogress.done();
+      if (error.response && error.response.status === 401) {
+          localStorage.clear();
+          if (window.location.pathname !== '/') {
+            window.location.href = '/';
+          }
+      }
+      return Promise.reject(error);
+    }
+);
+
+
+export default api;
